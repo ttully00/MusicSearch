546648eddd9c0433cc3b5c8450554d40ee377e3e
Tyrone Tully <tyrone.tully@ipsgroupinc.com>
2018-03-20 19:10:35 -0700

    changes

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,19 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-import Itunes from '@/components/Itunes'
+// import Itunes from '@/components/Itunes'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    // {
+    //   path: '/',
+    //   name: 'Itunes',
+    //   component: Itunes
+    // },
     {
       path: '/',
-      name: 'Itunes',
-      component: Itunes
-    },
-    {
-      path: '/hello',
       name: 'HelloWorld',
       component: HelloWorld
     }
