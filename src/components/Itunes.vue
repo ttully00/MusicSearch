1b1d7f48f05a8237f28ea7006b8f61fc4d265c69
Tyrone <tullyt@seattleu.edu>
2018-05-06 14:45:03 -0700

    JS

--- src/components/Itunes.vue
+++ src/components/Itunes.vue
@@ -1,65 +1,108 @@
-<template>
- <div class="itunes">
-    <h1>Itunes</h1>
-    <h2>Music and Video Streaming at its best!</h2>
+(function musicDB()
+ {
+  this.init = function()
+  {
 
-<form v-on:submit.prevent="findmusic">
-      <input type="text" id="myInput" placeholder="Search Favorite Artist.." title="Type in a name">
-     <input type="checkbox" name="enter name here" value="enter value here">Audio
-          <input type="checkbox" name="enter name here" value="enter value here">Video
- <p><input type="submit" value="Submit"></p>
-</form>
-<p>{{results}}</p>
-  </div>
+    this.search();
+  };
 
-</template>
+  /* Grab form, get input interact with database */
+  this.search = function()
+  {
+    var form = document.querySelector("#form");
 
-<script>
-import axios from "axios";
+    form.addEventListener("submit",function(e)
+                          {
+      e.preventDefault();//prevent form from resetting the page
+      //grab actual input to get value
+      var value = document.querySelector("#input_search").value;
 
-export default {
-  name: "Itunes",
-  data() {
-    return {
-      results: null,
-      errors: [],
-      entity: "",
-      atribute: "",
-      msg: "Itunes Search"
-    };
-  },
-  methods: {
-    findmusic: function() {
-      axios
-        .get("https://itunes.apple.com/search?term=R&B+HipHop&limit=25", {
-          params: { term: "R&B+HipHop", limit: "25" }
-        })
-        .then(response => {
-          this.results = response.data;
-        })
-        .catch(error => {
-          this.errors.push(error);
-        });
+      form.reset(); //reset the form after we grab the value
+
+      getData(value.split(' ').join("+"));
+
+    });
+  };//end search
+
+  /*
+Create a HTTP request
+*/
+  this.getData = function(artist)
+  {
+    var http = new XMLHttpRequest();
+    artist = artist.toString();
+    var url = "https://itunes.apple.com/search?term="+artist+"&entity=album";
+    var method = "GET";
+
+    //clear album container everytime
+    var container = document.querySelector('#album_list_container');
+    container.innerHTML = '';
+
+    http.open(method,url); //open a request
+
+    http.onreadystatechange = function()
+    {
+      if(http.readyState = 4 && http.status == 200 )
+      {
+        console.log("SUCCESS - connected to itunes API");
+
+        showArtist(JSON.parse(http.responseText));
+      }
+      else if(http.readyState == XMLHttpRequest.DONE && http.status != 200)
+      {
+        console.log("ERROR - connecting to Itunes API")
+      }
     }
-  }
-};
-</script>
+    http.send();
+  }; //end getdata
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1,
-h2 {
-  font-weight: normal;
+
+
+
+  this.showArtist=function(obj)
+  {
+    console.log(obj);
+    //grab container to host this
+    var container = document.querySelector("#album_list_container");
+    var template = '';
+    document.querySelector('#not_match').style.display="none";
+    if(obj.results.length > 0)
+    {
+
+      //there are albums
+      //loop through object results array and create the template
+      for(var i = 0; i< obj.results.length; i++)
+      {
+        template += '<div class = "col-sm-3 album_item">';
+
+        template += '<div class = "item_thmb" style = "background: url('+obj.results[i].artworkUrl100+')"></div>';
+
+        template += '<div class = "item_title"> '+obj.results[i].collectionName+'</div>';
+
+        template += '<div class = "item_price">';
+
+        template +='<span>Price:</span> '+obj.results[i].collectionPrice+' </div>';
+
+        template +='<a href = '+obj.results[i].collectionViewUrl+' target = "_blank">Buy now </a>';
+
+        template +='</div>';
+
+      }
+      container.innerHTML = '';
+      container.insertAdjacentHTML('afterbegin',template);
+    }
+    else
+    {
+      //no albums
+      document.querySelector('#not_match').style.display="block";
+      //alert("No albums");
+    }
+
+
+  }; //end showArtist
+
+
+
+  this.init();
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #010a06;
-}
-</style>
+)();
