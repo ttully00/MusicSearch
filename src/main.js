84857e8edb2b537a09b7ae225f4f67ce48bf5256
ttully00 <tullyt@seattleu.edu>
2018-06-17 20:18:43 -0700

    Boostrap attempt

--- src/main.js
+++ src/main.js
@@ -3,7 +3,12 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-//import Itunes from './Itunes.vue'
+
+import BootstrapVue from 'bootstrap-vue';
+Vue.use(BootstrapVue);
+
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 
 Vue.config.productionTip = false
 
